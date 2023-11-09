ad675dedd27d8521939047bcd5d0ca3b772c3867
Emanuel Coletti <emanuelcoletti001@gmail.com>
2023-11-09 15:00:45 -0300

    Commit inicial

---
+++ PrimerosPasos/src/Declaraciones_Operadores.java
@@ -0,0 +1,28 @@
+
+public class Declaraciones_Operadores {
+
+	public static void main(String[] args) {
+		
+		//double a=5;
+		
+		//double b;
+		
+		//b=10;
+		
+		//double c=b/a;
+		
+		//c-=6;
+		
+		final double apulgadas=2.54;
+		
+		double cm=6;
+		
+		double resultado=cm/apulgadas;
+		
+		
+		System.out.println("En " + cm + " cm hay " + resultado + " Pulgadas");
+		
+		
+	}
+
+}
---
+++ PrimerosPasos/src/Variables.java
@@ -0,0 +1,17 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		byte edad; // declaracion de una variable
+
+		edad = 35; // iniciacion de una variable
+
+		System.out.println(edad);
+
+		edad = 75;
+		System.out.println(edad);
+
+	}
+
+}
