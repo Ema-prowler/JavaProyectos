1af4169414aa900894f6155772874ec34bdbfa78
Emanuel Coletti <emanuelcoletti001@gmail.com>
2023-11-13 19:50:33 -0300

    aprendiendo manupulacion de cadenas

---
+++ PrimerosPasos/src/manipula_cadenas.java
@@ -0,0 +1,16 @@
+
+public class manipula_cadenas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre="Ema";
+		
+		System.out.println("Mi nombre es " + nombre);
+		
+		System.out.println("Mi nombre tiene " + nombre.length() + " letras.");
+		
+		System.out.println("La primera letra de " + nombre + " es la " + nombre.charAt(0));
+		
+	}
+
+}
