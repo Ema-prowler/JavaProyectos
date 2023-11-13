2b612d103fbe55a4476debc34fe7d8a659c9f5b3
Emanuel Coletti <emanuelcoletti001@gmail.com>
2023-11-13 19:51:39 -0300

    aprendiendo manupulacion de cadenas

--- PrimerosPasos/src/manipula_cadenas.java
+++ PrimerosPasos/src/manipula_cadenas.java
@@ -11,6 +11,12 @@ public class manipula_cadenas {
 		
 		System.out.println("La primera letra de " + nombre + " es la " + nombre.charAt(0));
 		
+		int ultima_letra;
+		
+		ultima_letra=nombre.length();
+		
+		System.out.println("Y la ultima letra es la " + nombre.charAt(ultima_letra-1));
+		
 	}
 
 }
