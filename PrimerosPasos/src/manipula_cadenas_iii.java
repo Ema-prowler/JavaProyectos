fcaeb74a2685a248e760b51403f8bccbcefc9420
Emanuel Coletti <emanuelcoletti001@gmail.com>
2023-11-15 19:38:30 -0300

    agregando el ultimo trabajo de manipulacion de cadenas

---
+++ PrimerosPasos/src/manipula_cadenas_iii.java
@@ -0,0 +1,18 @@
+
+public class manipula_cadenas_iii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String alumno1, alumno2;
+		alumno1="david";
+		alumno2="David";
+		
+		System.out.println(alumno1.equals(alumno2));
+		System.out.println(alumno1.equalsIgnoreCase(alumno2));
+		
+				
+		
+	}
+
+}
